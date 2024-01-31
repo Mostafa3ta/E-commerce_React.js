bf839b1e7776f36cbfad9722a324a6336a66fbe2
Mostafa-3ta <m2ostafa2000@gmail.com>
2024-02-01 01:22:18 +0200

    edits

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -13,22 +13,16 @@ export default function Footer() {
         <div className="container p-2 pb-0">
           {/* <div className='col-6'>Follow Us :</div> */}
           <section className="pb-2 ">
-            <Link className="btn btn-outline-light btn-floating m-1" to="#" role="button">
+            <Link className="btn btn-outline-light btn-floating m-1" to="https://www.facebook.com/profile.php?id=100007766405910&ref=xav_ig_profile_web" target='_blank' role="button">
               <i className="fab fa-facebook-f"></i></Link>
 
-            <Link className="btn btn-outline-light btn-floating m-1" to="#" role="button">
-              <i className="fab fa-twitter"></i></Link>
-
-            {/* <Link className="btn btn-outline-light btn-floating m-1" to="#" role="button">
-              <i className="fab fa-google"></i></Link> */}
-
-            <Link className="btn btn-outline-light btn-floating m-1" to="#" role="button">
+            <Link className="btn btn-outline-light btn-floating m-1" to="https://www.instagram.com/mostafa3ta_106?igsh=bXVoMm9yazcwdmlo" target='_blank' role="button">
               <i className="fab fa-instagram"></i></Link>
 
-            <Link className="btn btn-outline-light btn-floating m-1" to="#" role="button">
+            <Link className="btn btn-outline-light btn-floating m-1" to="https://www.linkedin.com/in/mostafa-mahmoud-33a1542b0" target='_blank' role="button">
               <i className="fab fa-linkedin-in"></i></Link>
 
-            <Link className="btn btn-outline-light btn-floating  m-1" to="#" role="button">
+            <Link className="btn btn-outline-light btn-floating  m-1" to="https://github.com/Mostafa3ta" target='_blank' role="button">
               <i className="fab fa-github"></i></Link>
           </section>
         </div>
