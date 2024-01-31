4f55d29324fa02976c49c1b4f0e98a0283f93da0
Mostafa-3ta <m2ostafa2000@gmail.com>
2024-01-31 23:46:18 +0200

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -15,26 +15,23 @@ export default function About() {
     <div className='my-4 col-md-6 about'>
       <div>
         <h3 className='text-success my-3 fw-bold'>About Cart :</h3 >
-        <div className='h5'>The wepsite have few things are not totally functional because the API is so
-          limited and used only for testing ,Specially the cart has a lot of issues because i cannot really
-          add into the server or modify it, so i imitated it by the local storage so at least
-          it can work, But you can see the result for some of the requests in the (console).
+        <div className='h5'> I used fake API store so I can't edit or add into Server DB
+        but i imiteted it with the local storage and simulate requests respnose in (console)
         </div>
       </div>
       <hr />
       <div>
         <h3 className='text-success my-3 fw-bold'>About Login :</h3 >
-        <div className='h5'>you cannot signup new user into the server it is just simulate the request and the response is
-          in the (console), And that's why there's a given username and password belongs to a user stored in
-          the database and when you login it respondes with a Token that you cannot access the wepsite without it .
+        <div className='h5'>you cannot signup new user into the server it simulates the request and the response is
+          in the (console), And there's a given username belongs to a user stored in
+          the database it respondes with a Token that you cannot access the wepsite without it .
         </div>
       </div>
       <hr />
       <div>
         <h3 className='text-success my-3 fw-bold'>About Products :</h3 >
         <div className='h5'> The products images from the API are all different in width and height which
-          ruins the whole User Experience ,So I had to give them a fixed height thats why some
-          pictures looks little stretched
+          is bad for User Experience ,So I had to give them a fixed height
         </div>
       </div>
       <hr />
